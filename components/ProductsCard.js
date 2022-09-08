a74b3ce54f7290bf94c3f43a3c7ccccb76876a3f
Jean Ferreira <jeanferreira91@hotmail.com>
2022-09-08 15:52:56 +0100

    HomeScreen has been polished to look better. Next is to be able to navigate through Categories

--- components/ProductsCard.js
+++ components/ProductsCard.js
@@ -1,6 +1,14 @@
-import { View, Text, ScrollView, Image, TouchableOpacity, Linking } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
+import { FireIcon } from "react-native-heroicons/solid";
 
 const ProductsCard = () => {
   // Query to fetch Products data from the API
@@ -19,32 +27,48 @@ const ProductsCard = () => {
       horizontal={false}
       showsVerticalScrollIndicator={true}
     >
-      <View className="flex-column items-center bg-white py-2 px-5 mb-36 space-y-4">
+      <View className="flex-column bg-white py-2 px-5 mb-36 space-y-4">
         {
-            // Looping though each product in the DB
-            data.map((item, index) => {
-                return (
-                    <View
-                        key={index}
-                        className="items-center justify-between pb-5 border-b-2 border-[#1f306e]"
-                    >
-                        <Image
-                            source={{ uri: item.item_pictures }}
-                            className="h-96 w-96"
-                        />
-                        <View className="items-start">
-                            <Text className="text-xl font-bold pb-2">{item.item_description}</Text>
-                            <TouchableOpacity
-                                className="rounded-lg bg-[#1f306e] p-4 ml-24"
-                                // Opening the product on the Browser
-                                onPress={() => Linking.openURL(item.url_amazon)}
-                            >
-                                <Text className="text-center text-white text-lg font-bold">View on Amazon</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>           
-                )
-            })
+          // Looping though each product in the DB
+          data.map((item, index) => {
+            return (
+              <View
+                key={index}
+                className="justify-between pb-5 border-b-2 border-[#1f306e]"
+              >
+                <View className="flex-row items-start justify-between">
+                  <Text className="bg-[#1f306e] text-lg font-bold text-white">{index + 1}</Text>
+                  {/* Checking if the item is special */}
+                  {item.is_especial ? (
+                    <FireIcon className="w-5 h-5" color={"#1f306e"} />
+                    ) : (
+                      <Text className=""></Text>
+                    )
+                  }
+                </View>
+                <View className="items-center pr-3 pl-3">
+                  <Image
+                    source={{ uri: item.item_pictures }}
+                    className="h-36 w-36 rounded"
+                  />
+                </View>
+                <View className="items-start">
+                  <Text className="text-sm font-bold pb-2">
+                    {item.item_description}
+                  </Text>
+                  <TouchableOpacity
+                    className="rounded-lg bg-[#1f306e] p-4 ml-24"
+                    // Opening the product on the Browser
+                    onPress={() => Linking.openURL(item.url_amazon)}
+                  >
+                    <Text className="text-center text-white text-md font-bold">
+                      View on Amazon
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            );
+          })
         }
       </View>
     </ScrollView>
